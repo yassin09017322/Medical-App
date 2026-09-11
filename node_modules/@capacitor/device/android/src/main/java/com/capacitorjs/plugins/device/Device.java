c68141849975c8f956e36cc2a6cce32d0ef1fc18
Yassin <ym8997914@gmail.com>
2026-09-11 00:51:45 -0700

    Update

---
+++ node_modules/@capacitor/device/android/src/main/java/com/capacitorjs/plugins/device/Device.java
@@ -0,0 +1,95 @@
+package com.capacitorjs.plugins.device;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.webkit.WebView;
+
+public class Device {
+
+    private Context context;
+
+    Device(Context context) {
+        this.context = context;
+    }
+
+    public long getMemUsed() {
+        final Runtime runtime = Runtime.getRuntime();
+        final long usedMem = runtime.totalMemory() - runtime.freeMemory();
+        return usedMem;
+    }
+
+    public String getPlatform() {
+        return "android";
+    }
+
+    public String getUuid() {
+        return Settings.Secure.getString(this.context.getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
+    public float getBatteryLevel() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = this.context.registerReceiver(null, ifilter);
+
+        int level = -1;
+        int scale = -1;
+
+        if (batteryStatus != null) {
+            level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        }
+
+        return level / (float) scale;
+    }
+
+    public boolean isCharging() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = this.context.registerReceiver(null, ifilter);
+
+        if (batteryStatus != null) {
+            int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            return status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
+        }
+        return false;
+    }
+
+    public boolean isVirtual() {
+        return Build.FINGERPRINT.contains("generic") || Build.PRODUCT.contains("sdk");
+    }
+
+    public String getName() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            return Settings.Global.getString(this.context.getContentResolver(), Settings.Global.DEVICE_NAME);
+        }
+
+        return null;
+    }
+
+    public String getWebViewVersion() {
+        PackageInfo info = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            info = WebView.getCurrentWebViewPackage();
+        } else {
+            try {
+                info = getWebViewVersionSubAndroid26();
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        if (info != null) {
+            return info.versionName;
+        }
+
+        return Build.VERSION.RELEASE;
+    }
+
+    private PackageInfo getWebViewVersionSubAndroid26() throws PackageManager.NameNotFoundException {
+        PackageManager pm = this.context.getPackageManager();
+        return pm.getPackageInfo("com.android.chrome", 0);
+    }
+}
---
+++ node_modules/@capacitor/device/android/src/main/java/com/capacitorjs/plugins/device/DevicePlugin.java
@@ -0,0 +1,71 @@
+package com.capacitorjs.plugins.device;
+
+import android.os.Build;
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+import java.util.Locale;
+
+@CapacitorPlugin(name = "Device")
+public class DevicePlugin extends Plugin {
+
+    private Device implementation;
+
+    @Override
+    public void load() {
+        implementation = new Device(getContext());
+    }
+
+    @PluginMethod
+    public void getId(PluginCall call) {
+        JSObject r = new JSObject();
+
+        r.put("identifier", implementation.getUuid());
+
+        call.resolve(r);
+    }
+
+    @PluginMethod
+    public void getInfo(PluginCall call) {
+        JSObject r = new JSObject();
+
+        r.put("memUsed", implementation.getMemUsed());
+        r.put("model", Build.MODEL);
+        r.put("operatingSystem", "android");
+        r.put("osVersion", Build.VERSION.RELEASE);
+        r.put("androidSDKVersion", Build.VERSION.SDK_INT);
+        r.put("platform", implementation.getPlatform());
+        r.put("manufacturer", Build.MANUFACTURER);
+        r.put("isVirtual", implementation.isVirtual());
+        r.put("name", implementation.getName());
+        r.put("webViewVersion", implementation.getWebViewVersion());
+
+        call.resolve(r);
+    }
+
+    @PluginMethod
+    public void getBatteryInfo(PluginCall call) {
+        JSObject r = new JSObject();
+
+        r.put("batteryLevel", implementation.getBatteryLevel());
+        r.put("isCharging", implementation.isCharging());
+
+        call.resolve(r);
+    }
+
+    @PluginMethod
+    public void getLanguageCode(PluginCall call) {
+        JSObject ret = new JSObject();
+        ret.put("value", Locale.getDefault().getLanguage());
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void getLanguageTag(PluginCall call) {
+        JSObject ret = new JSObject();
+        ret.put("value", Locale.getDefault().toLanguageTag());
+        call.resolve(ret);
+    }
+}
